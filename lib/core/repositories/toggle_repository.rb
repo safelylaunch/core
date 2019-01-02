64c06f9d170896a1c4b1bc0af1085e087baa0c0f
Anton Davydov <antondavydov.o@gmail.com>
2019-01-02 23:03:49 +0300

    Create a repository method for finding toggle for env

--- lib/core/repositories/toggle_repository.rb
+++ lib/core/repositories/toggle_repository.rb
@@ -4,4 +4,8 @@ class ToggleRepository < Hanami::Repository
   associations do
     belongs_to :environment
   end
+
+  def find_by_key_for_env(key, environment_id)
+    root.where(key: key, environment_id: environment_id).map_to(Toggle).one
+  end
 end
