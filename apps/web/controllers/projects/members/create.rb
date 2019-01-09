557d079da33935cc26f18e52e805bfd1f86be90a
Anton Davydov <antondavydov.o@gmail.com>
2019-01-09 09:42:28 +0300

    Create a simple logic for invite and remove members

--- apps/web/controllers/projects/members/create.rb
+++ apps/web/controllers/projects/members/create.rb
@@ -4,9 +4,22 @@ module Web
       module Members
         class Create
           include Web::Action
+          include Dry::Monads::Result::Mixin
+          include Import[operation: 'projects.operations.invite_member']
 
           def call(params)
-            redirect_to routes.project_path(params[:project_id])
+            result = operation.call(
+              project_id: params[:project_id],
+              email: params[:member][:email],
+              role: params[:member][:role]
+            )
+
+            case result
+            when Success
+              redirect_to routes.project_path(params[:project_id])
+            when Failure
+              redirect_to routes.project_path(params[:project_id])
+            end
           end
         end
       end
--- apps/web/controllers/projects/members/destroy.rb
+++ apps/web/controllers/projects/members/destroy.rb
@@ -4,9 +4,21 @@ module Web
       module Members
         class Destroy
           include Web::Action
+          include Dry::Monads::Result::Mixin
+          include Import[operation: 'projects.operations.remove_member']
 
           def call(params)
-            redirect_to routes.project_path(params[:project_id])
+            result = operation.call(
+              project_id: params[:project_id],
+              account_id: params[:id]
+            )
+
+            case result
+            when Success
+              redirect_to routes.project_path(params[:project_id])
+            when Failure
+              redirect_to routes.project_path(params[:project_id])
+            end
           end
         end
       end
