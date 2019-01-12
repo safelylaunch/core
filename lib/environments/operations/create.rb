e10289aef721968a748789cb108ae963ab046f8a
Anton Davydov <antondavydov.o@gmail.com>
2019-01-12 10:31:11 +0300

    Create a code for creating environment from operation

--- lib/environments/operations/create.rb
+++ lib/environments/operations/create.rb
@@ -9,6 +9,9 @@ module Environments
       ]
 
       def call(project_id:, account_id:, name:, color:)
+        Try(Hanami::Model::UniqueConstraintViolationError) do
+          env_repo.create(project_id: project_id, name: name, color: color, api_key: api_key_generator.call)
+        end.to_result
       end
     end
   end
--- spec/environments/operations/create_spec.rb
+++ spec/environments/operations/create_spec.rb
@@ -3,31 +3,57 @@
 RSpec.describe Environments::Operations::Create, type: :operation do
   let(:operation) { described_class.new(env_repo: env_repo, api_key_generator: api_key_generator) }
   let(:api_key_generator) { instance_double('Environments::Libs::ApiKeyGenerator', call: uuid) }
-  let(:env_repo) { instance_double('EnvironmentRepository') }
+  let(:env_repo) { instance_double('EnvironmentRepository', create: environment) }
   let(:uuid) { SecureRandom.uuid }
 
-  subject { operation.call(token: uuid) } 
+  subject { operation.call(project_id: 1, account_id: 1, name: 'stage', color: '000000') }
 
-  # TODO: specs
   context 'when account is a member of project and data is valid' do
+    let(:environment) { Environment.new(id: 1) }
+
+    it { expect(subject).to be_success }
+    it { expect(subject.value!).to eq(Environment.new(id: 1)) }
+
+    it 'calls api key generator' do
+      expect(env_repo).to receive(:create).with(project_id: 1, name: 'stage', color: '000000', api_key: uuid)
+      subject
+    end
   end
 
   context 'when account is not a member of project' do
-  end
-  
-  context 'when environment with same name exists' do
-  end
-
-  context 'when environment with same uuid exists' do
+    xit { expect(subject).to be_failure }
   end
 
   context 'with real dependencies' do
     let(:operation) { described_class.new }
+    let(:project) { Fabricate.create(:project) }
 
-    before { Fabricate.create(:environment, api_key: uuid) }
+    subject { operation.call(project_id: project.id, account_id: 1, name: 'stage', color: '000000') }
 
-    subject { operation.call(token: uuid) }
+    it { expect(subject).to be_success }
 
-    xit { expect(subject).to be_success }
+    context 'and environment with same api_key exist' do
+      let(:operation) { described_class.new(api_key_generator: api_key_generator) }
+      let(:api_key_generator) { instance_double('Environments::Libs::ApiKeyGenerator', call: uuid) }
+      let(:uuid) { SecureRandom.uuid }
+
+      subject { operation.call(project_id: project.id, account_id: 1, name: 'stage', color: '000000') }
+
+      before { Fabricate.create(:environment, api_key: uuid) }
+
+      it { expect(subject).to be_failure }
+    end
+
+    context 'and environment with same name exist in current project' do
+      before { Fabricate.create(:environment, project_id: project.id, name: 'stage') }
+
+      it { expect(subject).to be_failure }
+    end
+
+    context 'and environment with same name exist in other project' do
+      before { Fabricate.create(:environment, name: 'stage') }
+
+      it { expect(subject).to be_success }
+    end
   end
 end
