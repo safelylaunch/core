ec05f84753ea5fe72b3d61fef783c37a403fbd58
Anton Davydov <antondavydov.o@gmail.com>
2019-01-13 11:48:27 +0300

    Create a operation for creating toggles

--- lib/toggles/operations/create.rb
+++ lib/toggles/operations/create.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
-
 module Toggles
   module Operations
     class Create < Libs::Operation
       include Import[toggle_repo: 'repositories.toggle']
 
-      def call(key:, environment_id:)
+      Dry::Validation.load_extensions(:monads)
+
+      VALIDATOR = Dry::Validation.JSON do
+        required(:environment_id).filled(:int?)
+        required(:key).filled(:str?)
+        required(:name).filled(:str?)
+
+        optional(:description).maybe(:str?)
+
+        required(:type).filled(Core::Types::ToggleTypes)
+        required(:status).filled(Core::Types::ToggleStatuses)
+      end
+
+      def call(payload)
+        payload = yield VALIDATOR.call(payload).to_either
+
+        Try(Hanami::Model::UniqueConstraintViolationError) do
+          toggle_repo.create(payload)
+        end.to_result
       end
     end
   end
--- spec/toggles/operations/create_spec.rb
+++ spec/toggles/operations/create_spec.rb
@@ -3,14 +3,95 @@
 RSpec.describe Toggles::Operations::Create, type: :operation do
   let(:operation) { described_class.new(toggle_repo: toggle_repo) }
   let(:toggle_repo) { instance_double('ToggleRepository', create: toggle) }
+  let(:toggle) { Toggle.new(id: 1) }
 
-  subject { operation.call() }
+  subject { operation.call(payload) }
+
+  let(:payload) do
+    {
+      environment_id: 1,
+      key: 'enable-toggles',
+      name: 'test toggle',
+      description: 'have no idea what say here',
+      type: 'boolean',
+      status: 'enable'
+    }
+  end
+
+  context 'when data is valid and all is okay' do
+    it { expect(subject).to be_success }
+  end
+
+  context 'when data is invalid' do
+    context 'and status invalid' do
+      let(:payload) do
+        {
+          environment_id: 1,
+          key: 'enable-toggles',
+          name: 'test toggle',
+          description: 'have no idea what say here',
+          type: 'boolean',
+          status: 'invalid'
+        }
+      end
+
+      it { expect(subject).to be_failure }
+    end
+
+    context 'and type invalid' do
+      let(:payload) do
+        {
+          environment_id: 1,
+          key: 'enable-toggles',
+          name: 'test toggle',
+          description: 'have no idea what say here',
+          type: 'invalid',
+          status: 'enable'
+        }
+      end
+
+      it { expect(subject).to be_failure }
+    end
+
+    context 'and something missing' do
+      let(:payload) do
+        {
+          environment_id: 1,
+          name: 'test toggle',
+          description: 'have no idea what say here',
+          type: 'invalid',
+          status: 'enable'
+        }
+      end
+
+      it { expect(subject).to be_failure }
+    end
+  end
+
+  context 'when repository can not create a record' do
+    before do
+      allow(toggle_repo).to receive(:create).and_raise(Hanami::Model::UniqueConstraintViolationError)
+    end
+
+    it { expect(subject).to be_failure }
+  end
 
   context 'with real dependencies' do
     let(:operation) { described_class.new }
     let(:environment) { Fabricate.create(:environment) }
 
-    subject { operation.call() }
+    let(:payload) do
+      {
+        environment_id: environment.id,
+        key: 'enable-toggles',
+        name: 'test toggle',
+        description: 'have no idea what say here',
+        type: 'boolean',
+        status: 'enable'
+      }
+    end
+
+    subject { operation.call(payload) }
 
     it { expect(subject).to be_success }
   end
