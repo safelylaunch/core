4c0b37f1c98a56a98b44e01c4680f82b02b4dcd5
Anton Davydov <antondavydov.o@gmail.com>
2019-01-14 19:28:42 +0300

    Create a form for creating toggles

--- apps/web/views/toggles/new.rb
+++ apps/web/views/toggles/new.rb
@@ -3,6 +3,39 @@ module Web
     module Toggles
       class New
         include Web::View
+
+        def form
+          form_for :toggle, routes.environment_toggles_path(params[:environment_id]), method: :post do
+            text_field :environment_id, type: 'hidden', value: params[:environment_id]
+
+            div do
+              label :key
+              text_field :key, placeholder: 'new-toggle'
+            end
+
+            div do
+              label :name
+              text_field :name, placeholder: 'Name of your toggle'
+            end
+
+            div do
+              label :description
+              text_area :description, placeholder: 'optional'
+            end
+
+            div do
+              label :type
+              select :type, { 'Boolean' => 'boolean' }
+            end
+
+            div do
+              label :status
+              select :status, { 'Enable' => 'Enable', 'Disable' => 'disable' }
+            end
+
+            submit 'Create'
+          end
+        end
       end
     end
   end
